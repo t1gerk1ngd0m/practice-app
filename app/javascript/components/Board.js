4a37dcab7fb8483bac3d0a20591c80a43867a91a
Taiga Miwa <taiga.miwa888@gmail.com>
2020-07-09 18:38:46 +0900

    delete Bord handleClick

--- app/javascript/components/Board.js
+++ app/javascript/components/Board.js
@@ -2,18 +2,6 @@ import React from "react"
 import Square from "./Square.js"
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-
   renderSquare(i) {
     return (
       <Square
